9175a5a2ae28977f5c3ffc7bcd3e6f7f8e1ccccf
Emanuel Donalds <40231134+emanueldonalds@users.noreply.github.com>
2025-10-05 15:39:15 +0300

    do something in the test

--- src/test/java/edonalds/application/ListingControllerTest.java
+++ src/test/java/edonalds/application/ListingControllerTest.java
@@ -1,15 +1,53 @@
 package edonalds.application;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.http.HttpRequest.BodyPublishers;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
-import edonalds.App;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edonalds.model.Listing;
+import edonalds.model.Size;
+import jakarta.transaction.Transactional;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = App.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ListingControllerTest {
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mvc;
+
     @Test
-    void firstTest() {
+    void firstTimeRunAddsNewListings() throws Exception {
+        var listing = new Listing();
+        listing.setAgency("agency1");
+        listing.setName("");
+        listing.setAddress("");
+        listing.setUrl("");
+        listing.setPrice(100000);
+        listing.setSize(new Size(40f, "m2"));
+        listing.setBuildYear(1998);
+        listing.setMonthlyCharge(140f);
+        listing.setRooms(3);
+        listing.setVisibility(Arrays.asList("ALAND"));
+
+        mvc.perform(put("/listings")
+                .header("x-api-key", "123")
+                .header("Content-Type", "application/json")
+                .content(objectMapper.writeValueAsString(Arrays.asList(listing))))
+                .andExpect(status().isOk());
     }
 }
